546601934d112608b8baa521d5782c91cc8d9efb
Vangekillsvado <aclarke500@gmail.com>
2024-10-28 09:41:43 +0800

    removed font awesome dependcies

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import FontAwesomeIcon from "@/fontawesome-icons";
 
-createApp(App).component("FontAwesomeIcon", FontAwesomeIcon).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
